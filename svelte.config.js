4695bb6e6a42ced6a084ee701ac017959e302d62
Gareth Farrington <gareth@waves.ky>
2023-11-21 21:00:07 -0800

    Inject base path

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,11 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  dev: true,
   kit: {
 		adapter: adapter({
       pages: 'build',
 			paths: {
-        base: process.env.NODE_ENV === "production" ? "/PrusaSlicerPressureAdvanceCalibration" : "",
+        base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
       },
 		})
 	}
